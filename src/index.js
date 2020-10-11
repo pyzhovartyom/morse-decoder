afffdf735e000c035aafae50650eea81bab282e8
pyzhovartyom <pyzhovartyom@gmail.com>
2020-10-11 11:18:56 +0300

    feat: add morse decoder func

--- src/index.js
+++ src/index.js
@@ -38,9 +38,55 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = ``;
+    for (let i = 0; i < expr.length; i++) {
+        if (i % 10 === 9) {
+        result += expr[i] + ' ';
+        } else {
+        result += expr[i];
+        }
+    }
+    result = result.split(' ');
+    result.pop();
+    let resultNew = [];
+    for (let i = 0; i < result.length; i++) {
+        if (result[i] === '**********') {
+            resultNew.push(' ');
+        }
+        for (let j = 0; j < result[i].length; j++) {
+        if (result[i][j] === '0' && result[i][j + 1] !== '0') {
+            resultNew.push(result[i].slice(j + 1))
+            break
+        } else if (result[i][0] === '1') {
+          resultNew.push(result[i])
+          break
+        }
+      } 
+    }  
+    for (let i = 0; i < resultNew.length; i++) {
+        resultNew[i] = resultNew[i].match(/.{1,2}/g);
+    }
+    for (let i = 0; i < resultNew.length; i++) {
+        for (let j = 0; j < resultNew[i].length; j++) {
+        if (resultNew[i][j] === '10') {
+            resultNew[i][j] = '.';
+        } else if (resultNew[i][j] === '11') {
+            resultNew[i][j] = '-';
+        }  
+        }
+    }
+    result = resultNew.map(item => item.join(''));
+    resultNew = [];
+    for (i = 0; i < result.length; i++) {
+      if (result[i] === ' ') {
+        resultNew.push(' ');
+      } else if (MORSE_TABLE[result[i]]) {
+        resultNew.push(MORSE_TABLE[result[i]])
+      }
+    }
+    return resultNew.join('')
 }
-
+  
 module.exports = {
     decode
 }
